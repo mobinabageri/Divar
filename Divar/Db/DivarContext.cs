e70bc3b87780288f23287b32f9fbe0621c9619d8
mobinabageri <157510240+mobinabageri@users.noreply.github.com>
2024-11-17 19:00:16 +0330

    Add project files.

---
+++ Divar/Db/DivarContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Divar.Db;
+
+public partial class DivarContext : DbContext
+{
+    public DivarContext()
+    {
+    }
+
+    public DivarContext(DbContextOptions<DivarContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-59R0E40\\MOBINABAGHERI;Initial Catalog=Divar;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
